02fcda83fcae1fd0a8c0ff4cae4becacf37729ae
Ramish Hassan <ramishshah93@gmail.com>
2022-10-13 14:02:49 +0200

    interface Hello with logger complete,

--- handlers/hello.go
+++ handlers/hello.go
@@ -8,18 +8,21 @@ import (
 )
 
 type Hello struct{
-
+	l *log.Logger
 }
 
+func NewHello(l *log.Logger) *Hello {
+	return &Hello{l}
+}
 
 func (h*Hello) ServerHTTP(rw http.ResponseWriter, req *http.Request){
-	log.Println("Hello, world!")
+	h.l.Println("Hello, world!")
 	data, err := ioutil.ReadAll(req.Body)
-
-	fmt.Fprintf(rw, "hello %s\n", data)
 
 	if err!=nil {
 		http.Error(rw, "opps error", http.StatusBadRequest)
 		return
 	}
+
+	fmt.Fprintf(rw, "hello %s\n", data)
 }
