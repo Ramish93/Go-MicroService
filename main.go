540bede8788a02b65f56dffb082a795e150d2a12
Ramish Hassan <ramishshah93@gmail.com>
2022-10-13 12:29:38 +0200

    server responding on both routes

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ func main() {
 	// Hello world, the web server
 
 	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
-		log.Println("Hello, world!\n")
+		log.Println("Hello, world!")
 	})
 	
-	http.HandleFunc("/googby", func(http.ResponseWriter, *http.Request) {
-		log.Println("goodby, world!\n")
+	http.HandleFunc("/googby", func(http.ResponseWriter, *http.Request){
+		log.Println("goodby, world!")
 	})
+
+	http.ListenAndServe(":9090", nil)
 }
