25b0c9249227d8d8a15c53306b0cb636977ca2c7
Ramish Hassan <ramishshah93@gmail.com>
2022-10-13 16:26:27 +0200

    goodby.go file complete with /goodbye path, custom server in main.go file using context and timeout to emulate different scnerios

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 	"yt-go-microservice/handlers"
 )
 
@@ -17,7 +20,32 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", helloHandler)
 	mux.Handle("/goodbye", goodbyeHandler)
+// server:=
+	s:= &http.Server{
+		Addr: ":9090",
+		Handler: mux,
+		IdleTimeout: 120*time.Second,
+		ReadTimeout: 1*time.Second,
+		WriteTimeout: 1*time.Second,
+	}
 
-	
-	http.ListenAndServe(":9090", mux)
+	go func(){
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	//timeout Context
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
+	// http.ListenAndServe(":9090", mux)
 }
