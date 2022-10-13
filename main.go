ed9c6718ab7dda1d965c1b767b5710e2a1573ef1
Ramish Hassan <ramishshah93@gmail.com>
2022-10-13 15:08:03 +0200

    goodbye handler registered in mux and working

--- main.go
+++ main.go
@@ -9,11 +9,14 @@ import (
 
 func main() {
 	// Hello world, the web server
-	l:= log.New(os.Stdout, "product-api", log.LstdFlags)
+	l:= log.New(os.Stdout, "product-api", log.LstdFlags,)
+
 	helloHandler := handlers.NewHello(l)
+	goodbyeHandler := handlers.NewGoodbye(l)
 
 	mux := http.NewServeMux()
 	mux.Handle("/", helloHandler)
+	mux.Handle("/goodbye", goodbyeHandler)
 
 	
 	http.ListenAndServe(":9090", mux)
