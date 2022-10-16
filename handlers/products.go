cc5d62630aac65bc084b5bc7f9e69017de62b637
Ramish Hassan <ramishshah93@gmail.com>
2022-10-16 23:18:15 +0200

    fixed typos/bugs in handlers/products, was missing parameter and URI instead of URL

--- handlers/products.go
+++ handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 	"yt-go-microservice/data"
 )
 
@@ -24,13 +26,30 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		p.addProduct(rw,r)
 		return
 	}
-	//get id out of URI
-	// if r.Method == http.MethodPut {
-	// 	//expect Id in URL
-	// 	r := regexp.MustCompile(`/([0-9]+)`)
-	// 	r.FindAllStringSubmatch(r.URI.Path, -1)
+	// get id out of URI
+	if r.Method == http.MethodPut {
+		//expect Id in URI
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g:= reg.FindAllStringSubmatch(r.URL.Path, -1)
 
-	// }
+		if len(g) !=1{
+			http.Error(rw, "Invalid uri", http.StatusBadRequest)
+			return
+		}
+		if len(g[0]) !=1{
+			http.Error(rw, "Invalid uri", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Invalid uri", http.StatusBadRequest)
+			return
+		}
+
+		p.l.Println("got id:", id)
+	}
 
 	//catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
