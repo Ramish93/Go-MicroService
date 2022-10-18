32e0c40e2bd5d741211f8ccd23f5d9ff78a7fe91
Ramish Hassan <ramishshah93@gmail.com>
2022-10-18 20:42:43 +0200

    validation working on fields

--- data/products_test.go
+++ data/products_test.go
@@ -6,6 +6,7 @@ func TestChecksValidation(t *testing.T){
 	p := Product{
 		Name: "ice tea",
 		Price: 1.00,
+		SKU: "abs--shb",
 	}
 
 	err := p.Validate()
--- handlers/products.go
+++ handlers/products.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -68,7 +69,13 @@ func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
 			http.Error(rw, "Error reading product", http.StatusBadRequest)
 			return
 		}
-
+		//validate product
+		err = prod.Validate()
+		if err != nil {
+			p.l.Println("[ERROR] validating product", err)
+			http.Error(rw, fmt.Sprintf("Error validating product: %s",err ), http.StatusBadRequest)
+			return
+		}
 		// add the product to the context
 		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
 		r = r.WithContext(ctx)
