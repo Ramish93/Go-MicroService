3e367ac8999e2878d542e151d23639e16aabbd3d
Ramish Hassan <ramishshah93@gmail.com>
2022-10-18 20:12:55 +0200

    tests passing

--- data/products_test.go
+++ data/products_test.go
@@ -3,5 +3,14 @@ package data
 import "testing"
 
 func TestChecksValidation(t *testing.T){
-	
+	p := Product{
+		Name: "ice tea",
+		Price: 1.00,
+	}
+
+	err := p.Validate()
+
+	if err != nil {
+		t.Fatal(err)
+	}
 }
