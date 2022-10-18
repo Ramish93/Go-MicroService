16799ba883186534af9e915133680fb0dc55b15c
Ramish Hassan <ramishshah93@gmail.com>
2022-10-18 20:06:14 +0200

    function validate complete, name field required

--- data/products.go
+++ data/products.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"io"
 	"time"
+
+	"github.com/go-playground/validator"
 )
 
 // Product defines the structure for an API product
 type Product struct {
 	ID          int     `json:"id"`
-	Name        string  `json:"name"`
+	Name        string  `json:"name" validate:"required"`
 	Description string  `json:"description"`
 	Price       float32 `json:"price"`
 	SKU         string  `json:"sku"`
@@ -24,6 +26,10 @@ func (p *Product) FromJSON(r io.Reader) error{
 	return e.Decode(p)
 }
 
+func (p *Product) Validate()error {
+	validate := validator.New()
+	return validate.Struct(p)
+}
 
 type Products []*Product
 func (p *Products) ToJSON(w io.Writer) error {
