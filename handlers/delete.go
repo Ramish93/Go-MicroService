b03f019ffd52c6db8e07d570b51da25282160569
Ramish Hassan <ramishshah93@gmail.com>
2022-10-19 20:05:54 +0200

    delete route complete

--- handlers/delete.go
+++ handlers/delete.go
@@ -2,6 +2,10 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
+	"yt-go-microservice/data"
+
+	"github.com/gorilla/mux"
 )
 
 // swagger:route DELETE /products/{id} products deleteProduct
@@ -14,5 +18,19 @@ import (
 
 // Delete handles DELETE requests and removes items from the database
 func (p *Products) Delete(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id,_ := strconv.Atoi(vars["id"])
 	
+	p.l.Println("Handle delete request", id)
+
+	err := data.DeleteProduct(id)
+
+	if err == data.ErrProductNotFound{
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err!= nil {
+		http.Error(rw, "product not found", http.StatusInternalServerError)
+	}
 }
