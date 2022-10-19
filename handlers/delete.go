8a230ad5c10cefe98a8f0688dfef2bad9c029b72
Ramish Hassan <ramishshah93@gmail.com>
2022-10-19 19:58:20 +0200

    delete route build next, boiler plate code in file

---
+++ handlers/delete.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+// swagger:route DELETE /products/{id} products deleteProduct
+// Update a products details
+//
+// responses:
+//	201: noContentResponse
+//  404: errorResponse
+//  501: errorResponse
+
+// Delete handles DELETE requests and removes items from the database
+func (p *Products) Delete(rw http.ResponseWriter, r *http.Request) {
+	
+}
